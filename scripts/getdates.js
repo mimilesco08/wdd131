c5f770632a57f6be55130da0e7e6233422b2b688
mimilesco08 <eescobar@byupathway.edu>
2025-07-08 19:48:11 +0800

    W1 Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year and set it
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date and set it
+document.querySelector("#lastModified").textContent = "Last Modified: " + document.lastModified;
